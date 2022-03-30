4f1953b3dcab4fdac8aba9fe9bf2fd9e022abfe3
RUS826 <102674224+RUS826@users.noreply.github.com>
2022-03-31 00:57:42 +0530

    Update candidate.py

--- candidate.py
+++ candidate.py
@@ -1,29 +1,34 @@
-import csv
-with open("CandidateElimination.csv") as f:
-    csv_file = csv.reader(f)
-    data = list(csv_file)
-    s = data[1][:-1]
-    g = [['?' for i in range(len(s))] for j in range(len(s))]
-    for i in data:
-        if i[-1] == "Yes":
-            for j in range(len(s)):
-                if i[j] != s[j]:
-                    s[j] = '?'
-                    g[j][j] = '?'
-        elif i[-1] == "No":
-            for j in range(len(s)):
-                if i[j] != s[j]:
-                    g[j][j] = s[j]
-                else:
-                    g[j][j] = "?"
-        print("\nSteps of Candidate Elimination Algorithm", data.index(i) + 1)
-        print(s)
-        print(g)
-    gh = []
-    for i in g:
-        for j in i:
-            if j != '?':
-                gh.append(i)
-                break
-    print("\nFinal specific hypothesis:\n", s)
-    print("\nFinal general hypothesis:\n", gh)
+import numpy as np
+import pandas as pd
+data = pd.DataFrame(data=pd.read_csv('data.csv'))
+print('The Dataset is: \n')
+print(data)
+concepts = np.array(data)[:,0:-1]
+print('\n The Concepts are: \n',concepts)
+target = np.array(data)[:,-1]
+print('\nThe target is: \n',target)
+def learn(concepts, target):
+ specific_h = concepts[0].copy()
+ general_h = [["?" for i in range(len(specific_h))] for i in range(len(specific_h))]
+ for i, h in enumerate(concepts):
+ print("")
+ print(specific_h)
+ print(general_h)
+ if target[i] == "Yes":
+ for x in range(len(specific_h)):
+ if h[x] != specific_h[x]:
+ specific_h[x] = '?'
+ general_h[x][x] = '?'
+ if target[i] == "No":
+ for x in range(len(specific_h)):
+ if h[x] != specific_h[x]:
+ general_h[x][x] = specific_h[x]
+ else:
+ general_h[x][x] = '?'
+ indices = [i for i,val in enumerate(general_h) if val == ['?', '?', '?', '?', '?', '?']]
+ for i in indices:
+ general_h.remove(['?', '?', '?', '?', '?', '?'])
+ return specific_h, general_h
+s_final, g_final = learn(concepts, target)
+print("\n\nFinal S:", s_final)
+print("\n\nFinal G:", g_final)
